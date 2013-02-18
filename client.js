ab7d13a790347603e52c0d6d2fb2b6632e840764
John McLear <john@mclear.co.uk>
2013-02-18 17:33:01 +0000

    recording button

--- client.js
+++ client.js
@@ -1,11 +1,17 @@
 var eejs = require("ep_etherpad-lite/node/eejs");
 
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_email_notifications/templates/scripts.html", {}, module);
+  args.content = args.content + eejs.require("ep_narration/templates/scripts.html", {}, module);
   return cb();
 };
 
 exports.eejsBlock_embedPopup = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_email_notifications/templates/embedFrame.html", {}, module);
+  args.content = args.content + eejs.require("ep_narration/templates/embedFrame.html", {}, module);
   return cb();
 };
+
+exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_narration/templates/button.html", {}, module);
+  return cb();
+};
+
